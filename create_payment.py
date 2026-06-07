57fe6b481594c5221ca7f44fd6a7bf527ad4863c
Milagros Espejo <milagrosespejobo@gmail.com>
2026-06-07 12:58:11 -0400

    Week 4 Stripe - Customer object + linked to PaymentIntent

--- create_payment.py
+++ create_payment.py
@@ -6,13 +6,15 @@ stripe.api_key = os.environ.get("STRIPE_SECRET_KEY")
 intent = stripe.PaymentIntent.create(
     amount=2000,
     currency="usd",
+    customer="cus_Uf32v1uZmTUSmu",
     payment_method_types=["card"],
     metadata={
         "customer_name": "Milagros Espejo",
-        "order_id": "ORD-001"
+        "order_id": "ORD-002"
     }
 )
 
 print("Status:", intent.status)
 print("Intent ID:", intent.id)
+print("Customer:", intent.customer)
 print("Client secret:", intent.client_secret)
